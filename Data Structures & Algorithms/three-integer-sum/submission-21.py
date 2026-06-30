e0d2737d6b5317d39deb8a5f417e190b0f3f91d4
Alexander Stoilov <79380589+AlexanderStoilov@users.noreply.github.com>
2026-07-01 01:37:10 +0300

    Add: three-integer-sum - submission-21

---
+++ Data Structures & Algorithms/three-integer-sum/submission-21.py
@@ -0,0 +1,30 @@
+class Solution:
+    # VERY IMPORTANT: anchor on the smallest element because it uniquely identifies a triplet family in a sorted array
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # 3. two pointers
+        # time: O(n^2)
+        nums.sort()
+        res  = []
+        left = 0
+        # for left in range(len(nums)):
+        while left <= len(nums) - 1:
+            mid = left + 1
+            right = len(nums) - 1
+            while mid < right:
+                total = nums[left] + nums[mid] + nums[right]
+                if total > 0:
+                    right -= 1
+                elif total < 0:
+                    mid += 1
+                else:
+                    res.append([nums[left], nums[mid], nums[right]])
+                    mid += 1
+                    while mid < right and nums[mid] == nums[mid - 1]:
+                        mid += 1
+                    right -= 1
+                    while mid < right and nums[right] == nums[right + 1]:
+                        right -= 1
+            left += 1
+            while left <= len(nums) - 1 and nums[left] == nums[left - 1]:
+                left += 1
+        return res
